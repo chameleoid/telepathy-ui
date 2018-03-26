8a7638e724214295bb63596c09a6ae558faffa40
Kimberly Zick (rummik) <k@9k1.us>
2018-03-26 18:10:42 -0400

    Upload some ng-flex changes that have been forgotten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -32,6 +33,7 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    FlexLayoutModule,
 
     MatTooltipModule,
     MatSidenavModule,
